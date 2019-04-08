168825b234cff3e0111561067d73c66ebd4fd4c4
Bryan Luis Villafuerte <bryjho.villafuerte@yahoo.com.ph>
2019-04-08 18:05:16 +0200

    Add files via upload

---
+++ Assets/Stylesheets and Scripts/script.js
@@ -0,0 +1,62 @@
+function openMenu() {
+    var menuIcon = document.getElementById("rightmenu");
+    if (menuIcon.style.display === "block") {
+        menuIcon.style.display = "none";
+    } else {
+        menuIcon.style.display = "block";
+    }
+}
+
+function openFaqs1() {
+    var faq = document.getElementById("faqquestions1");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
+
+function openFaqs2() {
+    var faq = document.getElementById("faqquestions2");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
+
+function openFaqs3() {
+    var faq = document.getElementById("faqquestions3");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
+
+function openFaqs4() {
+    var faq = document.getElementById("faqquestions4");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
+
+function openFaqs5() {
+    var faq = document.getElementById("faqquestions5");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
+
+function openFaqs6() {
+    var faq = document.getElementById("faqquestions6");
+    if (faq.style.display === "block") {
+        faq.style.display = "none";
+    } else {
+        faq.style.display = "block";
+    }
+}
